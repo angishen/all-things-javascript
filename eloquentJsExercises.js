05285b5d5218444e1be5765aeafbe2275272afca
Angela Shen <angelashen01@gmail.com>
2018-09-05 14:29:25 -0700

    finish ch4 exercises and add book solutions ch2-4

--- eloquentJsExercises.js
+++ eloquentJsExercises.js
@@ -7,6 +7,13 @@ function buildTriangle(n) {
   }
 }
 
+// Book solution
+function buildTriangleBook() {
+  for (let line = "#"; line.length < 8; line += "#") {
+    console.log(line);
+  }
+}
+
 // 2. FizzBuzz
 function fizzBuzz() {
   for (let i = 0; i <= 100; i++) {
@@ -18,6 +25,16 @@ function fizzBuzz() {
       console.log("Buzz");
     }
   }
+}
+
+// Book solution
+function fizzBuzzBook() {
+  for (let n = 1; n <= 100; n++) {
+    let output = "";
+    if (n % 3 == 0) output += "Fizz";
+    if (n % 5 == 0) output += "Buzz";
+  }
+  console.log(output || n);
 }
 
 // 3. Chessboard
@@ -41,6 +58,23 @@ function buildChessboard(n) {
   return board;
 }
 
+// Book Solution
+function buildChessboardbook() {
+  let size = 8;
+  let board = "";
+  for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+      if ((x + y) % 2 == 0) {
+        board += " ";
+      } else {
+        board += "#";
+      }
+    }
+    board += "\n";
+  }
+  console.log(board);
+}
+
 // FUNCTIONS
 
 // 1. Minimum
@@ -48,6 +82,12 @@ function minimum(a, b) {
   if (a <= b) {
     return a;
   }
+  return b;
+}
+
+// Book solution
+function minBook(a, b) {
+  if (a < b) return a;
   return b;
 }
 
@@ -60,6 +100,14 @@ function isEven(num) {
     return false;
   }
   return isEven(num - 2);
+}
+
+// Book solution
+function isEvenBook(n) {
+  if (n == 0) return true;
+  if (n == 1) return false;
+  if (n < 0) return isEvenBook(-n);
+  return isEvenBook(n - 2);
 }
 
 // 3. Bean Counting
@@ -83,13 +131,33 @@ function countChars(str, char) {
   return numChars;
 }
 
+// Book solution
+// This is a case where you write a general function
+// and then have a more specific function call that general
+// function with specialized params
+function countCharBook(string, ch) {
+  let counted = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == ch) {
+      counted += 1;
+    }
+  }
+  return counted;
+}
+
+function countBsBook(string) {
+  return countCharBook(string, "B");
+}
+
 // DATA STRUCTURES
 
 // 1. Sum of a range
 function range(start, end, step = 1) {
   let result = [];
-  for (let i = start; i <= end; i += step) {
-    result.push(i);
+  if (start < end) {
+    for (let i = start; i <= end; i += step) result.push(i);
+  } else {
+    for (let i = start; i >= end; i -= step) result.push(i);
   }
   return result;
 }
@@ -98,6 +166,25 @@ function sum(arr) {
   return arr.reduce((acc, num) => {
     return acc + num;
   }, 0);
+}
+
+// Book solution
+function rangeBook(start, end, step = start < end ? 1 : -1) {
+  let array = [];
+
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) array.push(i);
+  } else {
+    for (let i = start; i >= end; i += step) array.push(i);
+  }
+}
+
+function sumBook(array) {
+  let total = 0;
+  for (let value of array) {
+    total += value;
+  }
+  return total;
 }
 
 // 2. Reverse and array
@@ -118,6 +205,124 @@ function reverseArrayInPlace(arr) {
   return arr;
 }
 
+// Book solution
+// Damn...my solutions for these were letter for letter same as the book's :D
+function reverseArrayBook(array) {
+  let output = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    output.push(array[i]);
+  }
+  return output;
+}
+
+function reverseArrayInPlaceBook(array) {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let old = array[i];
+    array[i] = array[array.length - i - 1];
+    array[array.length - i - 1] = old;
+  }
+  return array;
+}
+
 // 3. A list
+// yay recursion w/ ..rest
+// YASSS I'm super proud of this solution
+function arrayToList([first, ...rest]) {
+  if (!first) return null;
+  return { value: first, rest: arrayToList(rest) };
+}
 
+//  fxn(1, [2,3])
+//    return {value: 1, rest: fxn(2, [3])}
+//                              return {value: 2, rest: fxn(3, [] (null)}
+//                                                        base case: return null
 
+function listToArray(list, array = []) {
+  if (!list.rest) {
+    return [...array, list.value];
+  }
+  return listToArray(list.rest, [...array, list.value]);
+}
+
+//  let list = {
+//    value: 1,
+//    rest: {
+//      value: 2,
+//      rest: {
+//        value: 3,
+//        rest: null
+//      }
+//    }
+//  }
+
+//  fxn({value: 1, rest: {value: 2, rest: {value: 3, rest: null}}}, [])
+//    fxn({value: 2, rest: {value: 3, rest: null}, [1])
+//      fxn({value: 3, rest: null}, [1, 2])
+//        base case: return
+
+function prepend(value, list) {
+  return { value, rest: list };
+}
+
+function nth(list, n) {
+  if (!list) {
+    return undefined;
+  }
+  if (n === 0) {
+    return list.value;
+  }
+  return nth(list.rest, n - 1);
+}
+
+// Book solution
+function arrayToListBook(array) {
+  let list = null;
+  for (let i = array.length - 1; i >= 0; i--) {
+    list = { value: array[i], rest: list };
+  }
+  return list;
+}
+
+function listToArrayBook(list) {
+  let array = [];
+  for (let node = list; node; node = node.rest) {
+    array.push(node.value);
+  }
+  return array;
+}
+
+function prependBook(value, list) {
+  return { value, rest: list };
+}
+
+function nthBook(list, n) {
+  if (!list) return undefined;
+  if (n === 0) return list.value;
+  return nth(list.rest, n - 1);
+}
+
+// 4. Deep comparison
+function deepEqual(a, b) {
+  if (a === b) {
+    return true;
+  }
+
+  if (a === null || typeof a != 'object' ||
+      b === null || typeof b != 'object') {
+    return false;
+  }
+
+  let keysA = Object.keys(a), keysB = Object.keys(b);
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  for (let key of keysA) {
+    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) {
+      return false;
+    }
+  }
+  return true;
+
+}
